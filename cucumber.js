e18adba02ad4b6b5a500713f89f9d26986b1a901
fumichiba <fumihiro_chiba@post.pioneer.co.jp>
2025-06-02 18:12:35 +0900

    並列実行の無効化 プロファイル指定で使用ブラウザを切り替えられるように対応

--- cucumber.js
+++ cucumber.js
@@ -1,13 +1,33 @@
+const isCI = !!process.env.CI;
+
+const commonConfig = {
+  paths: ['tests/features/**/*.feature'],
+  require: ['tests/steps/**/*.ts', 'tests/support/**/*.ts'],
+  requireModule: ['ts-node/register'],
+  format: ['summary'],
+  retry: isCI ? 2 : 0,
+// テスト失敗時の動作が安定していないので並列実行は強制的にOFF
+//   parallel: isCI ? 0 : 4  // 0で並列OFF, 数字で同時実行数を制限
+  parallel: 0
+};
+
 module.exports = {
-    default: {
-        paths: ['tests/features/**/*.feature'],     // feature ファイルの場所
-        require: [                                  // テストスクリプトが格納される場所     
-            'tests/steps/**/*.ts',
-            'tests/support/**/*.ts'
-        ],
-        requireModule: ['ts-node/register'],        // TypeScript実行用の設定
-        format: [
-            'summary',
-        ],
-    }
-}
+  default: {
+    ...commonConfig,
+  },
+
+  chromium: {
+    ...commonConfig,
+    format: ['json:reports/json/chromium.json']
+  },
+
+  firefox: {
+    ...commonConfig,
+    format: ['json:reports/json/firefox.json']
+  },
+
+  webkit: {
+    ...commonConfig,
+    format: ['json:reports/json/webkit.json']
+  }
+};
---
+++ run-tests.ts
@@ -0,0 +1,9 @@
+import { execSync } from 'child_process';
+
+const isCI = process.env.CI === 'true';
+
+const command = isCI
+  ? 'npm-run-all --sequential --continue-on-error test:chromium test:firefox test:webkit'
+  : 'npm-run-all --parallel test:chromium test:firefox test:webkit';
+
+execSync(command, { stdio: 'inherit' });
