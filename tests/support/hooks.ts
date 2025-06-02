e4086c22d54d5de8524b3e511c69a5738ebf2138
fumichiba <fumihiro_chiba@post.pioneer.co.jp>
2025-06-02 13:07:43 +0900

    WSL環境で整形 テスト失敗時にスクリーンショット取得

--- tests/support/hooks.ts
+++ tests/support/hooks.ts
@@ -1,5 +1,5 @@
 // tests/support/hooks.ts
-import { Before, After, setDefaultTimeout } from '@cucumber/cucumber';
+import { Before, After, setDefaultTimeout, Status } from '@cucumber/cucumber';
 import { CustomWorld } from './custom-world';
 
 setDefaultTimeout(30 * 1000); // firefoxの起動時時間がかかるためタイムアウトを30秒に設定
@@ -8,6 +8,10 @@ Before(async function (this: CustomWorld) {
   await this.init(); // page, context, 各Pageオブジェクト初期化
 });
 
-After(async function (this: CustomWorld) {
+After(async function (this: CustomWorld, scenario) {
+  if (scenario.result?.status === Status.FAILED && this.page) {
+    const screenshot = await this.page.screenshot({ type: 'png' });
+    this.attach(screenshot, 'image/png');
+  }
   await this.close(); // cleanup
 });
