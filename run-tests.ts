ae58c9587c6d6a4d462712c89336ffd55ed00a8b
fumichiba <fumihiro_chiba@post.pioneer.co.jp>
2025-06-06 06:59:48 +0900

    テスト失敗時もレポート作成を必ず実行するように対応 わざとシナリオテストを失敗させるように変更

--- run-tests.ts
+++ run-tests.ts
@@ -6,9 +6,23 @@ const command = isCI
   ? 'npm-run-all --sequential --continue-on-error test:chromium test:firefox test:webkit'
   : 'npm-run-all --parallel --continue-on-error test:chromium test:firefox test:webkit';
 
+let hasError = false;
+
+// テストを実行
 try {
   execSync(command, { stdio: 'inherit' });
 } catch (e) {
   console.warn('テストの一部が失敗しました。');
+  hasError = true;
+}
+
+// レポート生成は必ず行う
+try {
+  execSync('npm run report', { stdio: 'inherit' });
+} catch (e) {
+  console.warn('レポート生成に失敗しました。');
+}
+
+if (hasError) {
   process.exit(1);
 }
