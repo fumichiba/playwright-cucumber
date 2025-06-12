7b9caaca09959131b3c48347270f2f1c7750da68
fumichiba <fumihiro_chiba@post.pioneer.co.jp>
2025-06-12 09:00:35 +0900

    playwright環境構築(playwright+Cucumber環境との併用)

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,7 +12,9 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './tests',
+  testDir: './playwright-tests',
+  // Folder for test artifacts such as screenshots, videos, traces, etc.
+  outputDir: 'playwright-results',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
